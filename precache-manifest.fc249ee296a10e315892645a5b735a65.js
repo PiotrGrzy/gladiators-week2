7d624fe195b671053268f06864d2575610c6cbfb
PiotrGrzy <48014334+PiotrGrzy@users.noreply.github.com>
2020-05-22 23:48:43 +0200

    Updates

---
+++ precache-manifest.fc249ee296a10e315892645a5b735a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49473c6daf83a06e35fe1990fd33561b",
+    "url": "/gladiators-week2/index.html"
+  },
+  {
+    "revision": "58be54a01e2324113b0a",
+    "url": "/gladiators-week2/static/css/main.41e3c151.chunk.css"
+  },
+  {
+    "revision": "6479b2a5e200fd027731",
+    "url": "/gladiators-week2/static/js/2.68778175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gladiators-week2/static/js/2.68778175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58be54a01e2324113b0a",
+    "url": "/gladiators-week2/static/js/main.f6e330cd.chunk.js"
+  },
+  {
+    "revision": "0207e79581d3b58057bd",
+    "url": "/gladiators-week2/static/js/runtime-main.98d42d39.js"
+  }
+]);
